c2c16d4d1375855e5b645b0c6faed75a28584d42
Gabriele <gabriele@bonsai-studio.net>
2014-05-21 13:24:02 +0200

    Updated license

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name='django-image-tools',
-      version='0.6b3',
+      version='0.6.b4',
       description='Image tools for Django',
       author='Bonsai Studio',
       author_email='info@bonsai-studio.net',
       url='http://github.com/bonsaistudio/django-image-tools',
       packages=['django_image_tools', 'django_image_tools.migrations'],
-     )
+      )
