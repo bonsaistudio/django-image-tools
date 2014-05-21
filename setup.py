c8ea08671bb42899cbff0214da7689813f71325c
Gabriele <gabriele@bonsai-studio.net>
2014-05-21 13:02:39 +0200

    Added b3 version with migrations

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name='django-image-tools',
-      version='0.6.1.b',
+      version='0.6b3',
       description='Image tools for Django',
       author='Bonsai Studio',
       author_email='info@bonsai-studio.net',
       url='http://github.com/bonsaistudio/django-image-tools',
-      packages=['django_image_tools', ],
+      packages=['django_image_tools', 'django_image_tools.migrations'],
      )
