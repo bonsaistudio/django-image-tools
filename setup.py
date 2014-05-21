7b57c7c586ba9947dcc9f0c6b8be05b628e83002
Gabriele <gabriele@bonsai-studio.net>
2014-05-21 13:30:55 +0200

    Updated setup.py with license, read me tags and other info

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 from distutils.core import setup
+import os
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='django-image-tools',
       version='0.6.b5',
@@ -6,5 +14,15 @@ setup(name='django-image-tools',
       author='Bonsai Studio',
       author_email='info@bonsai-studio.net',
       url='http://github.com/bonsaistudio/django-image-tools',
+      license='BSD',
+      keywords='django image filter tools resize crop',
+      long_description=read('README.rst'),
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Framework :: Django',
+          'Environment :: Web Environment',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python',
+      ],
       packages=['django_image_tools', 'django_image_tools.migrations'],
       )
